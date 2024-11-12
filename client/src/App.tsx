ef104a4b91223ffd9c0de3a3dd7b67b47ce4e8a3
Sharika999 <153902791+Sharika999@users.noreply.github.com>
2024-11-12 19:40:54 +0530

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import Clock from './components/Clock';
+import AlarmManager from './components/AlarmManager';
+
+function App() {
+  return (
+    <div className='bg-gray-100 sm:p-6 font-orbitron'>
+      <Clock />
+      <AlarmManager />
+    </div>
+  );
+}
+
+export default App;
